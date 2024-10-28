09ad2f2bdc71d5ce0d33dfcd6348897b61f65cfb
MincerGithub <849380859@qq.com>
2024-10-29 01:27:03 +0800

    有时候爬不了

--- dianfei.py
+++ dianfei.py
@@ -5,10 +5,35 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 import pandas as pd
+import json
+import urllib
+import requests
 
 
 def get_num():
-    return 1
+    url = 'http://10.100.1.24:8988/web/Common/Tsm.html'
+    header = {
+        'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36 Edg/130.0.0.0',
+        'Host':'10.100.1.24:8988',
+        'X-Requested-With':'XMLHttpRequest',
+        'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        'Connection': 'keep-alive'
+    }
+    # a = {
+    #     "jsondata":{ "query_elec_building": { "aid": "0030000000002505", "account": "823767", "area": {"area": "青岛校区", "areaname": "青岛校区"  } } },
+    #     "funname":"synjones.onecard.query.elec.building",
+    #     "json":"true"
+    # }                                                        换指定房间的，指定账号的
+    b = json.dumps(a)
+    data = urllib.parse.quote(b)
+    response = requests.post(url=url, data=data, headers=header)
+    base_data = response.json()
+    data = base_data["query_elec_roominfo"]["errmsg"]
+    num = data[8:]
+    return num
 
 
 def send_email():
@@ -37,9 +62,11 @@ def judge():
         send_email()
 
 
-schedule.every().hour.at(":00").do(judge())
+if __name__ == '__main__':
+    judge()
 
+    schedule.every().hour.at(":00").do(judge())
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
---
+++ 扒的响应.py
@@ -0,0 +1,159 @@
+a = {
+    "query_elec_roominfo": {
+        "retcode": "0",
+        "errmsg": "房间当前剩余电量349.40",
+        "aid": "0030000000002505",
+        "account": "823767",
+        "meterflag": "amt",
+        "bal": "",
+        "price": "0",
+        "pkgflag": "none",
+        "area": {
+            "area": "青岛校区",
+            "areaname": "青岛校区"
+        },
+        "building": {
+            "buildingid": "1503975890",
+            "building": "凤凰居2号楼"
+        },
+        "floor": {
+            "floorid": "",
+            "floor": ""
+        },
+        "room": {
+            "roomid": "a325",
+            "room": "a325"
+        },
+        "pkgtab": []
+    }
+}
+
+
+
+
+
+
+
+
+b = {
+    "query_elec_building": {
+        "retcode": "0",
+        "errmsg": "请选择相应的楼栋：",
+        "aid": "0030000000002505",
+        "account": "823767",
+        "area": {
+            "area": "青岛校区",
+            "areaname": "青岛校区"
+        },
+        "buildingtab": [
+            {
+                "buildingid": "1503975980",
+                "building": "凤凰居6号楼"
+            },
+            {
+                "buildingid": "1661835273",
+                "building": "B5号楼"
+            },
+            {
+                "buildingid": "1661835256",
+                "building": "B2"
+            },
+            {
+                "buildingid": "1574231830",
+                "building": "T1"
+            },
+            {
+                "buildingid": "1503975832",
+                "building": "凤凰居1号楼"
+            },
+            {
+                "buildingid": "1503975832",
+                "building": "S1一多书院"
+            },
+            {
+                "buildingid": "1599193777",
+                "building": "S11"
+            },
+            {
+                "buildingid": "1693031698",
+                "building": "B9"
+            },
+            {
+                "buildingid": "1503976004",
+                "building": "凤凰居9号楼"
+            },
+            {
+                "buildingid": "1503975890",
+                "building": "凤凰居2号楼"
+            },
+            {
+                "buildingid": "1503975967",
+                "building": "S5凤凰居5号楼"
+            },
+            {
+                "buildingid": "1503976037",
+                "building": "凤凰居10号楼"
+            },
+            {
+                "buildingid": "1503975890",
+                "building": "S2从文书院"
+            },
+            {
+                "buildingid": "1693031710",
+                "building": "阅海居B10楼"
+            },
+            {
+                "buildingid": "1693031698",
+                "building": "阅海居B9楼"
+            },
+            {
+                "buildingid": "1574231835",
+                "building": "T3"
+            },
+            {
+                "buildingid": "1503976004",
+                "building": "S9凤凰居9号楼"
+            },
+            {
+                "buildingid": "1503975988",
+                "building": "S7凤凰居7号楼"
+            },
+            {
+                "buildingid": "1503976037",
+                "building": "S10凤凰居10号楼"
+            },
+            {
+                "buildingid": "1503975995",
+                "building": "S8凤凰居8号楼"
+            },
+            {
+                "buildingid": "1599193777",
+                "building": "凤凰居11/13号楼"
+            },
+            {
+                "buildingid": "1574231833",
+                "building": "专家公寓2号楼"
+            },
+            {
+                "buildingid": "1503975902",
+                "building": "凤凰居3号楼"
+            },
+            {
+                "buildingid": "1693031710",
+                "building": "B10"
+            },
+            {
+                "buildingid": "1661835249",
+                "building": "B1"
+            },
+            {
+                "buildingid": "1503975950",
+                "building": "凤凰居4号楼"
+            },
+            {
+                "buildingid": "1503975980",
+                "building": "S6凤凰居6号楼"
+            }
+        ]
+    }
+}
